71b123c3ab60c019ae6c4d55a72f3c005b6ed149
soyoung2022 <yuns8671@gmail.com>
2025-02-26 13:52:32 +0900

    [FIX] EC2 public ip 오류 - origins

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # 요청을 허용할 출처 목록
+    allow_origins=["*"],  # 요청을 허용할 출처 목록
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 (GET, POST, PUT 등) 허용
     allow_headers=["*"],  # 모든 헤더 허용
