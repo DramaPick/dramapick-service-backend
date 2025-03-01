a2895ae65b610d73dd4682157329fa1dddd55dc5
soyoung2022 <yuns8671@gmail.com>
2025-03-02 01:18:24 +0900

    새로운 Dockerfile (2)

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ load_dotenv()
 app = FastAPI()
 
 origins = [
+    "https://dramapick.site",
     "http://dramapick-deploy-test.s3-website.ap-northeast-2.amazonaws.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000"
